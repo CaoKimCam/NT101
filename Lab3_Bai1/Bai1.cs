30e361740a821038372254602ac235fca66a8b64
CaoKimCam <21520641@gm.uit.edu.vn>
2023-10-31 11:47:20 +0700

    Add project files.

---
+++ Lab3_Bai1/Bai1.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab3_Bai1
+{
+    public partial class Bai1 : Form
+    {
+        public Bai1()
+        {
+            InitializeComponent();
+        }
+        //Hàm xử lý input đầu vào 
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Data_TextChanged(object sender, EventArgs e)
+        {
+            //dùng hàm xử lý input
+        }
+
+        private void txt_Key_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_MD5_TextChanged(object sender, EventArgs e)
+        {
+            //chuỗi băm MD5
+        }
+
+        private void txt_SHA1_TextChanged(object sender, EventArgs e)
+        {
+            //chuỗi băm SHA1
+        }
+
+        private void txt_SHA3_TextChanged(object sender, EventArgs e)
+        {
+            //chuỗi băm SHA3
+        }
+    }
+}
